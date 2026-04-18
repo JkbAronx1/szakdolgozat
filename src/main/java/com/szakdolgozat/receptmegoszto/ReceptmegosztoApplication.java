1a296fce7ac626439c8f7e1c422387e1ef7bbf13
JkbAronx1 <aron.jk76@gmail.com>
2026-04-18 19:00:52 +0200

    Spring Boot alapok hozzáadása

---
+++ src/main/java/com/szakdolgozat/receptmegoszto/ReceptmegosztoApplication.java
@@ -0,0 +1,13 @@
+package com.szakdolgozat.receptmegoszto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptmegosztoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptmegosztoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szakdolgozat/receptmegoszto/ReceptmegosztoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szakdolgozat.receptmegoszto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptmegosztoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
