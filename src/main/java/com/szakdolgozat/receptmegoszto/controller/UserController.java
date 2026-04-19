90fdb276740d3c4743af1d36ecb3204ef79d0c61
JkbAronx1 <aron.jk76@gmail.com>
2026-04-20 00:15:35 +0200

    bejelentkezés létrehozása

--- src/main/java/com/szakdolgozat/receptmegoszto/controller/UserController.java
+++ src/main/java/com/szakdolgozat/receptmegoszto/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.szakdolgozat.receptmegoszto.controller;
+
 import com.szakdolgozat.receptmegoszto.entity.User;
 import com.szakdolgozat.receptmegoszto.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -25,5 +28,21 @@ public class UserController {
         userRepository.save(ujFelhasznalo);
 
         return "Sikeres regisztráció!";
+    }
+
+    @PostMapping("/api/login")
+    public String login(@RequestParam String email, @RequestParam String password) {
+
+        Optional<User> felhasznalo = userRepository.findByEmail(email);
+
+        if (felhasznalo.isPresent()) {
+            if (felhasznalo.get().getPassword().equals(password)) {
+                return "Sikeres bejelentkezés!";
+            } else {
+                return "Hibás jelszó!";
+            }
+        } else {
+            return "Hiba: Ezzel az email címmel még senki sem regisztrált!";
+        }
     }
 }
--- src/main/java/com/szakdolgozat/receptmegoszto/repository/UserRepository.java
+++ src/main/java/com/szakdolgozat/receptmegoszto/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.szakdolgozat.receptmegoszto.repository;
 import com.szakdolgozat.receptmegoszto.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
